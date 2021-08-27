541022d9c5028f83b76c557cc21ab0a0dc045843
zs <zs@bjpowernode.com>
2021-08-28 00:43:49 +0800

    1.3.4

--- src/main/java/com/bjpowernode/crm/settings/service/impl/UserServiceImpl.java
+++ src/main/java/com/bjpowernode/crm/settings/service/impl/UserServiceImpl.java
@@ -25,28 +25,29 @@ public class UserServiceImpl implements UserService {
         User user = userDao.login(map);
 
         if(user==null){
+
             throw new LoginException("账号密码错误");
+
         }
 
         //验证失效时间
         String expireTime = user.getExpireTime();
         String currentTime = DateTimeUtil.getSysTime();
         if(expireTime.compareTo(currentTime)<0){
-            throw new LoginException("账号已失效");
-        }
 
+            throw new LoginException("账号已失效");
+
+        }
         //判断锁定状态
         String lockState = user.getLockState();
         if("0".equals(lockState)){
             throw new LoginException("账号已锁定");
         }
-
         //判断ip地址
         String allowIps = user.getAllowIps();
         if(!allowIps.contains(ip)){
             throw new LoginException("ip地址受限");
         }
-
         return user;
     }
 
