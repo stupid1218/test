85fcfcafcf35c032948ea0e164283be438e91347
zs <zs@bjpowernode.com>
2021-08-29 14:31:27 +0800

    1.3.4

--- src/main/java/com/bjpowernode/crm/web/filter/EncodingFilter.java
+++ src/main/java/com/bjpowernode/crm/web/filter/EncodingFilter.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 
 public class EncodingFilter implements Filter {
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
         System.out.println("进入到过滤字符编码的过滤器");
 
         //过滤post请求中文参数乱码
-        req.setCharacterEncoding("UTF-8");
+        servletRequest.setCharacterEncoding("UTF-8");
         //过滤响应流响应中文乱码
-        resp.setContentType("text/html;charset=utf-8");
+        servletResponse.setContentType("text/html;charset=utf-8");
 
         //将请求放行
-        chain.doFilter(req, resp);
+        filterChain.doFilter(servletRequest, servletResponse);
 
     }
 }
